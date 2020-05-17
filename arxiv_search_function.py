f9ba3d68bbd0d10fafff172c79f0c1862210ce82
Blair Bilodeau <blair.bilodeau@mail.utoronto.ca>
2020-05-17 15:15:40 -0400

    Added search files

---
+++ arxiv_search_function.py
@@ -0,0 +1,274 @@
+#####
+# Created by Blair Bilodeau
+# Last modified May 11, 2020
+
+#####
+# Inspired by:
+# https://pypi.org/project/arxiv-checker/
+# https://github.com/mahdisadjadi/arxivscraper (more useful)
+
+#####
+# Resources:
+# https://arxiv.org/help/api
+# https://arxiv.org/help/oa/index
+# http://www.openarchives.org/OAI/openarchivesprotocol.html
+
+######################################################################
+
+## packages
+import pandas as pd
+import xml.etree.ElementTree as ET
+import datetime
+import time
+import sys
+import string
+import gc
+import requests
+from urllib.parse import urlencode
+from urllib.request import urlopen
+from urllib.error import HTTPError
+
+#######
+## Main function
+## Parameters:
+# sdate 	   - datetime object 		- starting date of search period 
+# fdate 	   - datetime object 		- end date of search period
+# kwd_req	   - list of str            - keywords that are required to be in the title or abstract
+# kwd_exc	   - list of str            - keywords that must not be in the title or abstract
+# kwd_one	   - list of lists of str   - keywords of which at least one must be included
+# athr_req	   - list of str            - authors which are required
+# athr_exc	   - list of str            - authors to exclude
+# athr_one	   - list of lists of str   - authors of which one must be included
+# subjects	   - list of str 			- subject options are:
+#											astro-ph, cond-mat, gr-qc, hep-ex, hep-lat, hep-ph, hep-th, math-ph, nlin, nucl-ex, nucl-th, physics, quant-ph,
+#											math, cs, q-bio, q-fin, stat
+# max_records  - int             		- maximum number of results to return
+# max_time 	   - float           		- maximum amount of seconds to be spent searching 
+# cols 		   - list            		- arxiv fields to extract
+#				  							column options are:
+#				  							id, url, title, authors, date, abstract, categories
+# export       - str             		- folder location to dump results list in csv
+# exportfile   - str			 		- by default the file name will be today's date, but you can override this with exportfile
+# download     - str             		- folder location to dump returned pdfs into
+#								 			(files named using year_lastname format)
+
+def arxivsearch(start_date = datetime.date.today().replace(day=1), 
+	end_date = datetime.date.today(), 
+	kwd_req = [], 
+	kwd_exc = [], 
+	kwd_one = [],
+	athr_req = [], 
+	athr_exc = [], 
+	athr_one = [], 
+	subjects = [], 
+	max_records = 50, 
+	max_time = 300,
+	cols = ['id', 'title', 'authors', 'date', 'categories', 'abstract'],
+	export = '',
+	exportfile = '',
+	download = ''):
+
+	## keep track of timing
+	overall_time = time.time()
+
+	## urls
+	OAI = '{http://www.openarchives.org/OAI/2.0/}'
+	ARXIV = '{http://arxiv.org/OAI/arXiv/}'
+	BASE = 'http://export.arxiv.org/oai2?verb=ListRecords&'
+
+	## format dates
+	start_date = str(start_date)
+	end_date = str(end_date)
+
+	## format inputs
+	kwd_req = set([keyword.lower() for keyword in kwd_req]) 
+	kwd_exc = set([keyword.lower() for keyword in kwd_exc]) 
+	kwd_one = [set([keyword.lower() for keyword in kwd_one_list]) for kwd_one_list in kwd_one]
+
+	athr_req = set([author.lower() for author in athr_req]) 
+	athr_exc = set([author.lower() for author in athr_exc]) 
+	athr_one = [set([author.lower() for author in athr_one_list]) for athr_one_list in athr_one]  
+
+	## dataframe to store results
+	if download != '' and 'date' not in cols:
+		cols = cols + ['date'] # make sure date is stored if downloading PDFs
+	records_df = pd.DataFrame(columns=cols)
+
+	## loop over all subjects
+	for subject in subjects:
+
+		subject_time = time.time()
+
+		# set url to extract papers from this subject
+		subject = subject.lower()
+		url = BASE + 'from=' + start_date + '&until=' + end_date + '&metadataPrefix=arXiv&set=%s' %subject
+
+		## OAI only lets you parse 1000 records at a time, so loop through these until parsed all records
+		k = 1
+		while True:
+
+			# keep user aware of status
+			print('Searching records {:d} to {:d} from {:s}...'.format(1000 * (k-1) + 1, 1000*k, str(subject))) 
+
+			## error handling for opening and closing URL
+			try:
+				url_response = urlopen(url)
+				xml = url_response.read() # get raw xml data from server
+				gc.collect()
+			except HTTPError as e:
+				# handle service unavailable error for requesting too often
+				if e.code == 503:
+					retry_time = int(e.hdrs.get('retry-after', 30))
+					print('Got 503. Retrying after {0:d} seconds.'.format(retry_time))
+					time.sleep(retry_time + 1)
+					continue
+				# if it's a different error, just have to raise it
+				else:
+					raise
+
+			k += 1
+
+			xml_root = ET.fromstring(xml) # get root of xml hierarchy
+			records = xml_root.findall(OAI + 'ListRecords/' + OAI + 'record') # list of all records from xml tree
+
+			## extract metadata for each record
+			metadata = [record.find(OAI + 'metadata').find(ARXIV + 'arXiv') for record in records]
+
+			## use metadata to get info for each record
+			titles = [meta.find(ARXIV + 'title').text.strip().lower().replace('\n', ' ') for meta in metadata]
+			dates = [meta.find(ARXIV + 'created').text.strip() if meta.find(ARXIV + 'updated') is None else meta.find(ARXIV + 'updated').text.strip() for meta in metadata]
+			ids = [meta.find(ARXIV + 'id').text.strip().lower().replace('\n', ' ') for meta in metadata]
+			abstracts = [meta.find(ARXIV + 'abstract').text.strip().lower().replace('\n', ' ') for meta in metadata]
+			category_lists = [meta.find(ARXIV + 'categories').text.strip().lower().replace('\n', ' ').split() for meta in metadata]
+			urls = ['https://arxiv.org/abs/' + meta.find(ARXIV + 'id').text.strip().lower().replace('\n', ' ') for meta in metadata]
+			author_lists = [meta.findall(ARXIV + 'authors/' + ARXIV + 'author') for meta in metadata]
+
+			## extract first and last names, clean them, and put them together to make human readable
+			last_name_lists = [[author.find(ARXIV + 'keyname').text.lower() for author in author_list] for author_list in author_lists]
+			first_name_meta_lists = [[author.find(ARXIV + 'forenames') for author in author_list] for author_list in author_lists]
+			first_name_lists = [['' if name == None else name.text.lower() for name in first_name_meta_list] for first_name_meta_list in first_name_meta_lists]
+			full_name_temp_lists = [zip(a,b) for a,b in zip(first_name_lists, last_name_lists)]
+			full_name_lists = [[a+' '+b for a,b in full_name_temp_list] for full_name_temp_list in full_name_temp_lists]
+
+			## compile all info into big dataframe
+			records_data = list(zip(titles, dates, ids, abstracts, category_lists, urls, full_name_lists))
+			temp_df = pd.DataFrame(records_data,columns=['title','date','id','abstract','categories','url','authors'])
+
+			### want to find only indices that correspond to desired papers
+		
+			## merge abstracts and titles into one big text blob without punctuation to search for keywords in
+			abstract_title_concats = [title+' '+abstract for title,abstract in zip(titles,abstracts)]
+			translator = str.maketrans(string.punctuation, ' '*len(string.punctuation))
+			abstract_title_sets = [set(abstract_title_concat.translate(translator).split()) for abstract_title_concat in abstract_title_concats]
+			
+			### Choose indices to match keywords and authors
+
+			## only abstracts and titles that have intersection with required categories
+			if len(kwd_one) > 0:
+				# for each kwd_one list take indexes such that title/abstract intersect with the list
+				kwd_one_idxs_lists = [set([idx for idx,val in enumerate(list(map(len,map(lambda x: x.intersection(kwd_one_list), abstract_title_sets)))) if val > 0]) for kwd_one_list in kwd_one]
+				# take intersection of all the kwd_one index sets
+				kwd_one_idxs = kwd_one_idxs_lists[0].intersection(*kwd_one_idxs_lists) 
+			else:
+				kwd_one_idxs = set(range(len(abstract_title_sets)))
+			if len(kwd_req) > 0:
+				kwd_req_idxs = set([idx for idx,val in enumerate(list(map(len,map(lambda x: x.intersection(kwd_req), abstract_title_sets)))) if val == len(kwd_req)])
+			else:
+				kwd_req_idxs = set(range(len(abstract_title_sets)))
+			if len(kwd_exc) > 0:
+				kwd_exc_idxs = set([idx for idx,val in enumerate(list(map(len,map(lambda x: x.intersection(kwd_exc), abstract_title_sets)))) if val == 0])
+			else:
+				kwd_exc_idxs = set(range(len(abstract_title_sets)))
+
+			## intersection of all keyword indices
+			# annoying intersection syntax makes you pick a set to apply method to, arbitrarily chose kwd_one
+			kwd_idxs = kwd_one_idxs.intersection(kwd_req_idxs, kwd_exc_idxs)
+
+			## consider sets of author names to intersect with
+			full_name_sets = [set(full_name_list) for full_name_list in full_name_lists]
+
+			## only authors that are specified as desired
+			if len(athr_one) > 0:
+				# for each athr_one list take indexes such that author list intersects with the list
+				athr_one_idxs_lists = [set([idx for idx,val in enumerate(list(map(len,map(lambda x: x.intersection(athr_one_list), full_name_sets)))) if val > 0]) for athr_one_list in athr_one]
+				# take intersection of all the athr_one index sets
+				athr_one_idxs = athr_one_idxs_lists[0].intersection(*athr_one_idxs_lists) 
+			else:
+				athr_one_idxs = set(range(len(full_name_sets)))
+			if len(athr_req) > 0:
+				athr_req_idxs = set([idx for idx,val in enumerate(list(map(len,map(lambda x: x.intersection(athr_req), full_name_sets)))) if val == len(athr_req)])
+			else:
+				athr_req_idxs = set(range(len(full_name_sets)))
+			if len(athr_exc) > 0:
+				athr_exc_idxs = set([idx for idx,val in enumerate(list(map(len,map(lambda x: x.intersection(athr_exc), full_name_sets)))) if val == 0])
+			else:
+				athr_exc_idxs = set(range(len(full_name_sets)))
+
+			## intersection of all author indices
+			# annoying intersection syntax makes you pick a set to apply method to, arbitrarily chose athr_one
+			athr_idxs = athr_one_idxs.intersection(athr_req_idxs, athr_exc_idxs)
+			
+			## only take temp_df rows that match the desired indices for keywords and authors
+			temp_df = temp_df.iloc[list(kwd_idxs.intersection(athr_idxs))]
+
+			## merge new results into dataframe with old results, while dropping duplicate records
+			records_df = pd.concat([records_df,temp_df],sort=True).drop_duplicates(subset='title', keep="first").reset_index(drop=True)[cols]
+
+			### Ease of use handling
+
+			## see if too much time has passed
+			if time.time() - overall_time > max_time:
+				print('Max time ({:.0f} seconds) reached. Fetched {:d} records from {:s} in {:.1f} seconds.'.format(max_time, len(records_df), str(subjects), time.time() - overall_time))
+				return(records_df)
+
+			## see if more records than desired have been pulled
+			if len(records_df) > max_records:
+				print('Max number of records ({:d}) reached. Fetched {:d} records from {:s} in {:.1f} seconds.'.format(max_records, len(records_df), str(subjects), time.time() - overall_time))
+				return(records_df)
+
+			## see if there are more records to pull in this category by checking resumptionToken
+			try:
+				token = xml_root.find(OAI + 'ListRecords').find(OAI + 'resumptionToken')
+			except:
+				break
+			if token is None or token.text is None:
+				break
+			else:
+				url = BASE + 'resumptionToken=%s' % token.text
+
+		## keep user informed on how much time this subject has taken
+		print('Fetching from {:s} is completed in {:.1f} seconds.'.format(str(subject), time.time() - subject_time))
+	
+	## keep user informed on how much time the whole process took
+	print('Fetched {:d} records from {:s} in {:.1f} seconds.'.format(len(records_df), str(subjects), time.time() - overall_time))
+
+	## check if user wants to export this dataframe
+	if export != '':
+		if exportfile == '':
+			exportfile = datetime.date.today().strftime('%Y-%m-%d') + '-arxiv-metadata.csv'
+		exportpath = export + exportfile
+		print('Exporting records as csv to {:s}...'.format(exportpath))
+		records_df.to_csv(exportpath, index=False)
+		print('Export complete.')
+
+	## check if user wants to download the pdfs
+	if download != '':
+		print('Downloading {:d} PDFs to {:s}...'.format(len(records_df), download))
+		pdf_urls = ['https://arxiv.org/pdf/' + id + '.pdf' for id in records_df.id] # list of urls to pull pdfs from
+
+		# create filenames to export pdfs to
+		# currently setup in year_lastname format
+		pdf_lastnames_full = ['_'.join([name.split()[-1] for name in namelist]) for namelist in records_df.authors] # pull out lastnames only
+		pdf_lastnames = [name if len(name) < 200 else name.split('_')[0] + '_et_al' for name in pdf_lastnames_full] # make sure file names don't get longer than ~200 chars
+		pdf_paths = [download + date + '_' + lastname + '.pdf' for date,lastname in zip(records_df.date, pdf_lastnames)] # full path for each file
+		# export pdfs
+		for paper_idx in range(len(pdf_urls)):
+			response = requests.get(pdf_urls[paper_idx])
+			file = open(pdf_paths[paper_idx], 'wb')
+			file.write(response.content)
+			file.close()
+			gc.collect()
+		print('Download complete.')
+
+	## return dataframe with records and quit
+	return(records_df)
---
+++ biorxiv_search_function.py
@@ -0,0 +1,250 @@
+#####
+# Created by Blair Bilodeau
+# Last modified May 16, 2020
+
+#####
+# Resources:
+# https://www.biorxiv.org/search
+
+######################################################################
+
+## packages
+import math
+import pandas as pd
+import datetime
+import time
+import sys
+import string
+import gc
+import requests
+from bs4 import BeautifulSoup as bs
+
+#######
+## Main function
+## Parameters:
+# sdate 	   - datetime object 		- starting date of search period 
+# fdate 	   - datetime object 		- end date of search period
+# journal	   - str                    - either biorxiv, medarxiv, or both
+# kwd	   	   - list of str            - keywords to search for in title and abstract
+# kwd_type	   - 'all' or 'any'         - whether all keywords are required or just one of them
+# athr	   	   - list of max 2 str      - authors which are required
+# subjects	   - list of str 			- subject options (only available if journal is biorxiv) are:
+#										  (Note capitalization and spacing are important for subject)
+# 											Animal Behaviour and Cognition, Biochemistry, Bioengineering, Bioinformatics,
+#											Biophysics, Cancer Biology, Cell Biology, Clinical Trials, Developmental Biology,
+#											Ecology, Epidemiology, Evolutionary Biology, Genetics, Genomics, Immunology,
+#											Microbiology, Molecular Biology, Neuroscience, Paleontology, Pathology, 
+#											Pharmacology and Toxicology, Physiology, Plant Biology, Scientific Communication and Education,
+#											Synthetic Biology, Systems Biology, Zoology
+# max_records  - int             		- maximum number of results to return
+# max_time 	   - float           		- maximum amount of seconds to be spent searching 
+# cols 		   - list            		- biorxiv fields to extract
+#				  							column options are:
+# 				  							title, authors, date, url
+# abstract     - bool  					- whether to extract the abstract of every paper returned by search 
+#										  	(potentially very time consuming)
+# export       - str             		- folder location to dump results list in csv
+# exportfile   - str			 		- by default the file name will be today's date, but you can override this with exportfile
+# download     - str             		- folder location to dump returned pdfs into
+#								 			(files named using year_lastname format)
+
+def biorxivsearch(start_date = datetime.date.today().replace(day=1), 
+	end_date = datetime.date.today(), 
+	journal = 'biorxiv',
+	subjects = [], 
+	kwd = [], 
+	kwd_type = 'all', 
+	athr = [], 
+	max_records = 75, 
+	max_time = 300,
+	cols = ['title', 'authors', 'date', 'url'],
+	abstracts = False,
+	export = '',
+	exportfile = '',
+	download = ''):
+
+	## keep track of timing
+	overall_time = time.time()
+
+	## url
+	BASE = 'http://biorxiv.org/search/'
+	url = BASE
+
+	## format dates
+	start_date = str(start_date)
+	end_date = str(end_date)
+
+	## format inputs
+	journal = journal.lower()
+	kwd_type = kwd_type.lower()
+
+	### build the url string
+
+	## journal selection
+	if journal == 'biorxiv':
+		journal_str = 'jcode%3Abiorxiv'
+	elif journal == 'medrxiv':
+		journal_str = 'jcode%3Amedrxiv'
+	else:
+		journal_str = 'jcode%3Amedrxiv%7C%7Cbiorxiv'
+
+	url += journal_str
+
+	## subject selection
+	if journal == 'biorxiv' and len(subjects)>0:
+		first_subject = ('%20').join(subjects[0].split())
+		subject_str = 'subject_collection_code%3A' + first_subject
+		for subject in subjects[1:]:
+			subject_str = subject_str + '%2C' + ('%20').join(subject.split())
+		url += '%20' + subject_str
+
+	## keyword selection
+	if len(kwd) > 0:
+		kwd_str = 'abstract_title%3A' + ('%252C%2B').join([kwd[0]] + [('%2B').join(keyword.split()) for keyword in kwd[1:]])
+		kwd_str = kwd_str + '%20abstract_title_flags%3Amatch-' + kwd_type
+		url += '%20' + kwd_str
+
+	## author selection
+	if len(athr) == 1:
+		athr_str = 'author1%3A' + ('%2B').join(athr[0].split())
+		url += '%20' + athr_str
+	if len(athr) == 2:
+		athr_str = 'author1%3A' + ('%2B').join(athr[0].split()) + '%20author2%3A' + ('%2B').join(athr[1].split())
+		url += '%20' + athr_str
+
+	## date range string
+	date_str = 'limit_from%3A' + start_date + '%20limit_to%3A' + end_date
+	url += '%20' + date_str
+
+	## fixed formatting
+	num_page_results = 75
+	url += '%20numresults%3A' + str(num_page_results) + '%20format_result%3Acondensed' + '%20sort%3Arelevance-rank'
+
+	## lists to store date
+	titles = []
+	author_lists = []
+	urls = []
+	dates = []
+
+	### once the string has been built, access site
+
+	# initialize number of pages to loop through
+	page = 0
+
+	## loop through other pages of search if they exist
+	while True:
+
+		# keep user aware of status
+		print('Fetching search results {:d} to {:d}...'.format(num_page_results*page+1, num_page_results*(page+1)))
+
+		# access url and pull html data
+		if page == 0:
+			url_response = requests.post(url)
+			html = bs(url_response.text)
+
+			# find out how many results there are, and make sure don't pull more than user wants
+			num_results = int(html.find('div', attrs={'class': 'highwire-search-summary'}).text.strip().split()[0])
+			num_fetch_results = min(max_records, num_results)
+		
+		else:
+			page_url = url + '?page=' + str(page)
+			url_response = requests.post(page_url)
+			html = bs(url_response.text)
+
+		# list of articles on page
+		articles = html.find_all(attrs={'class': 'search-result'})
+
+		## pull details from each article on page
+		titles += [article.find('span', attrs={'class': 'highwire-cite-title'}).text.strip() if article.find('span', attrs={'class': 'highwire-cite-title'}) is not None else None for article in articles]
+		author_lists += [[author.text for author in article.find_all('span', attrs={'class': 'highwire-citation-author'})] for article in articles]
+		
+		journals = [article.find('div', attrs={'class': 'highwire-article-citation highwire-citation-type-highwire-article'}).get('data-pisa').strip().split(';')[0] for article in articles]
+		url_stubs = [article.find('a', href=True)['href'] for article in articles]
+		# url link is different depending on medrxiv or biorxiv
+		urls += ['http://www.biorxiv.org' + url_stub if journal == 'biorxiv' else url_stub for journal, url_stub in zip(journals, url_stubs)]
+
+		dates += [('-').join(article.find('div', attrs={'class': 'highwire-article-citation highwire-citation-type-highwire-article'}).get('data-pisa').strip().split(';')[1].split('.')[0:3]) for article in articles]
+
+		## see if too much time has passed or max number of records reached or no more pages
+		if time.time() - overall_time > max_time or (page+1)*num_page_results >= num_fetch_results:
+			break
+
+		page += 1
+
+	## only consider desired number of results
+	records_data = list(zip(*list(map(lambda dummy_list: dummy_list[0:num_fetch_results], [titles, author_lists, urls, dates]))))
+	full_records_df = pd.DataFrame(records_data,columns=['title', 'authors', 'url', 'date'])
+
+	## keep user informed on why task ended
+	if num_results > max_records:
+		print('Max number of records ({:d}) reached. Fetched in {:.1f} seconds.'.format(max_records, time.time() - overall_time))
+	elif time.time() - overall_time > max_time:
+		print('Max time ({:.0f} seconds) reached. Fetched {:d} records in {:.1f} seconds.'.format(max_time, num_fetch_results, time.time() - overall_time))
+	else:
+		print('Fetched {:d} records in {:.1f} seconds.'.format(num_fetch_results, time.time() - overall_time))
+	
+	## check if abstracts are to be pulled
+	if abstracts:
+		print('Fetching abstracts for {:d} papers...'.format(len(full_records_df)))
+		full_records_df['abstract'] = [bs(requests.post(paper_url).text).find('div', attrs={'class': 'section abstract'}).text.replace('Abstract','').replace('\n','') for paper_url in full_records_df.url]
+		cols += ['abstract']
+		print('Abstracts fetched.')
+
+	## check if paper should be downloaded
+	if download != '':
+		print('Downloading {:d} PDFs to {:s}...'.format(len(full_records_df), download))
+		pdf_urls = [url + '.full.pdf' for url in full_records_df.url] # list of urls to pull pdfs from
+
+		# create filenames to export pdfs to
+		# currently setup in year_lastname format
+		pdf_lastnames_full = ['_'.join([name.split()[-1] for name in namelist]) for namelist in full_records_df.authors] # pull out lastnames only
+		pdf_lastnames = [name if len(name) < 200 else name.split('_')[0] + '_et_al' for name in pdf_lastnames_full] # make sure file names don't get longer than ~200 chars
+		pdf_paths = [download + date + '_' + lastname + '.pdf' for date,lastname in zip(full_records_df.date, pdf_lastnames)] # full path for each file
+		# export pdfs
+		for paper_idx in range(len(pdf_urls)):
+			response = requests.get(pdf_urls[paper_idx])
+			file = open(pdf_paths[paper_idx], 'wb')
+			file.write(response.content)
+			file.close()
+			gc.collect()
+		print('Download complete.')
+
+	## create dataframe to be returned
+	records_df = full_records_df[cols]
+
+	## check if user wants to export this dataframe
+	if export != '':
+		if exportfile == '':
+			exportfile = datetime.date.today().strftime('%Y-%m-%d') + '-biorxiv-metadata.csv'
+		exportpath = export + exportfile
+		print('Exporting records as csv to {:s}...'.format(exportpath))
+		records_df.to_csv(exportpath, index=False)
+		print('Export complete.')
+	
+	print('Total time to fetch and manipulate records was {:.1f} seconds.'.format(time.time() - overall_time))
+
+	## return the results
+	return(records_df)
+
+records_df = biorxivsearch(start_date = datetime.date.today().replace(day=1), 
+	end_date = datetime.date.today().replace(day=6), 
+	journal = 'both',
+	subjects = ['Biochemistry'], 
+	kwd = ['covid', 'stem cell'], 
+	kwd_type = 'any',
+	abstracts=True,
+	athr = [], 
+	max_records = 10,
+	max_time = 300)
+
+# records_df = biorxivsearch(start_date = datetime.date.today().replace(day=1), 
+# 	end_date = datetime.date.today().replace(day=6), 
+# 	journal = 'both',
+# 	subjects = ['Biochemistry'], 
+# 	kwd = [], 
+# 	kwd_type = 'any',
+# 	athr = ['charlotte', 'matthew jones'], 
+# 	max_records = 300,
+# 	max_time = 300)
+
+print(records_df.title)
