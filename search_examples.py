95fbdce89da07116e858a83e7e0e74755981f740
Blair Bilodeau <blair.bilodeau@mail.utoronto.ca>
2020-05-23 16:37:24 -0400

    Added examples and shell script to run them.

--- search_examples.py
+++ search_examples.py
@@ -3,8 +3,8 @@
 # Last modified May 22, 2020
 
 #####
-# Examples of running arxiv_search_function.py
-# Place file in the same folder as arxiv_search_function.py
+# Examples of running arxiv_search_function.py and biorxiv_search_function.py
+# Place file in the same folder as function files
 # Can run each individually, or comment out all but 1 to run from command line
 
 import arxiv_search_function as asf
@@ -24,14 +24,15 @@ records_df = asf.arxivsearch(
 
 records_df2 = asf.arxivsearch(
 	start_date = datetime.date.today().replace(day=1), 
- 	end_date = datetime.date.today(), 
+ 	end_date = datetime.date.today().replace(day=22), 
 	subjects = ['cs','stat'], 
 	kwd_req = ['GAN', 'unsupervised'], 
 	kwd_exc = [], 
 	kwd_one = [['theory', 'impossibility', 'sample complexity', 'ycleGAN']], 
 	max_records = 50, 
 	max_time = 300,
-	cols = ['id', 'title', 'categories', 'abstract', 'authors', 'date'])
+	cols = ['id', 'title', 'categories', 'abstract', 'authors', 'date'],
+	export = '/Users/blairbilodeau/Desktop/arxiv/')
 
 records_df3 = bsf.biorxivsearch(
 	start_date = datetime.date.today().replace(day=1), 
